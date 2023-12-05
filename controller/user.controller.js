7e8794fc9819927cd3af161cbd5059df8a4ef8ba
anbuleo <anbuliyon@gmail.com>
2023-12-05 14:54:20 +0530

    delete user functionality

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -46,7 +46,8 @@ const deleteUser = async(req,res,next) => {
     if(req.user.id !== req.params.id) return next(errorHandler(401, 'You can only delete your own account '))
     try {
         await User.findByIdAndDelete(req.params.id)
-        res.status(200).json('user has been deleted').clearCookie('access_token')
+        res.clearCookie('access_token')
+        res.status(200).json('user has been deleted')
     } catch (error) {
 
         next(error)
