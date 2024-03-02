6fae303c760bd0101d298bcb2f7ae819279c854e
anbuleo <anbuliyon@gmail.com>
2024-03-02 17:27:07 +0530

    trust_proxy_sameSite

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -135,7 +135,7 @@ export const google = async (req,res,next) => {
         if(user){
             const token = jwt.sign({id: user._id},process.env.JWT_SECRET)
             const {password:pass, ...rest} = user._doc
-            res.cookie('access_token',token,{httpOnly:false,sameSite: 'none'}).status(200).json(rest)
+            res.cookie('access_token',token,{httpOnly:false,sameSite: 'none',secure:true}).status(200).json(rest)
         }
         else{
             const generatedPassword =Math.random().toString(36).slice(-8) + Math.random().toString(36).slice(-8);
@@ -144,7 +144,7 @@ export const google = async (req,res,next) => {
             await newUser.save()
             const token = jwt.sign({id: user._id},process.env.JWT_SECRET)
             const {password:pass, ...rest} = user._doc
-            res.cookie('access_token',token,{httpOnly:false,sameSite: 'none'}).status(200).json(rest)
+            res.cookie('access_token',token,{httpOnly:false,sameSite: 'none',secure:true}).status(200).json(rest)
 
         }
     } catch (error) {
