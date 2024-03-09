8cc2e9115045eedb212f032758fa91079b8967e5
anbuleo <anbuliyon@gmail.com>
2024-03-09 13:44:47 +0530

    site

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -133,7 +133,7 @@ export const signIn = async (req,res,next) => {
         res.cookie('access_token', token, {
     httpOnly: true, // The cookie cannot be accessed through client-side scripts
     secure: true, // Send the cookie only over HTTPS (in a production environment)
-    sameSite: 'Strict', // Protect against CSRF attacks
+    sameSite: 'none', // Protect against CSRF attacks
     expires: new Date(Date.now() + 3600000), // Cookie expiration time (1 hour in milliseconds)
   }).status(200).json(rest)
         // res.cookie('access_token',token,{httpOnly:true}).status(200).json(rest)
