75d85a0c499ed3a5da60deaf04971596f1302dbc
anbuleo <anbuliyon@gmail.com>
2024-03-09 13:54:31 +0530

    token

--- utils/verifyUser.js
+++ utils/verifyUser.js
@@ -3,10 +3,11 @@ import { errorHandler } from './error.js';
 
 
 export const verifyToken = (req,res,next) => {
-    // let token = req.get('cookie').split("=")[1] //another way of parser the cookie in req
+    let tokens = req.get('cookie').split("=")[1] //another way of parser the cookie in req
     
     const token = req.cookies.access_token; // after installed npm package npm i cookie-parser
-    // console.log(token)
+    console.log(token,tokens)
+
     
     if(!token) return next(errorHandler(401, 'Unauthorised'))
 
